a0fb6417434fab97462fed57f3c9ad3aeb06675c
Puzaan Xhres <41267289+puzaan@users.noreply.github.com>
2021-06-02 10:43:58 +0545

    Created header footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Footer from './component/Footer';
+import Header from './component/Header';
 
 function App() {
   return (
     <>
+    <Header/>
 <h1> Well come to </h1>
+<Footer/>
     </>
   );
 }
