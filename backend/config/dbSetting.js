95baef1957804114ddca6d4a4cd5e08cef70db06
Puzaan Xhres <41267289+puzaan@users.noreply.github.com>
2021-06-03 13:25:37 +0545

    connect with mongoDB

---
+++ backend/config/dbSetting.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+
+const connectDB = async () =>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex:true,
+        });
+        console.log(`MongoDB Connected ${conn.connection.host}`)
+        
+    } catch (error) {
+        console.error(`Error: ${err.message}`);
+        process.exit(1);
+        
+    }
+}
+
+export default connectDB
+
+// process.exit(1 === true); = exit whole app 
--- backend/server.js
+++ backend/server.js
@@ -13,15 +13,16 @@ import express from 'express';
 import products from './data/products.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './config/dbSetting.js'
 
 
-
-
-const app = express();
 
 dotenv.config();
 
 
+connectDB();
+
+const app = express();
 // to run both server
 app.use(cors());
 
