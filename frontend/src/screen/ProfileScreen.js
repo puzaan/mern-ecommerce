a8a108531d6bc081272f42f683b4852bea88304b
Puzaan Xhres <41267289+puzaan@users.noreply.github.com>
2021-06-30 09:08:57 +0545

    show user profile detail to edit

--- frontend/src/screen/ProfileScreen.js
+++ frontend/src/screen/ProfileScreen.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col, Form } from "react-bootstrap";
+import { Row, Col, Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserDetails } from "../actions/userAction";
-
+import Loader from "../components/Loder";
+import Message from '../components/Message'
 
 const ProfileScreen = ({ history }) => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [message, setMessage] = useState("");
 
     const userDetails = useSelector((state) => state.userDetails);
     const userLogin = useSelector((state) => state.userLogin);
@@ -24,26 +28,73 @@ const ProfileScreen = ({ history }) => {
                 dispatch(getUserDetails());
             } else {
                 setName(user.name);
-                setEmail(user.email)
+                setEmail(user.email);
+                setPassword(user.password);
             }
         }
     }, [history, userInfo, user, dispatch]);
+
+    const submitHandler = (e) => {
+
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            setMessage("Password are not matched");
+        } else {
+            // dispatch updata profile
+        }
+    };
     return (
         <Row>
             <Col md={3}>
                 <h2>User Profile</h2>
-                <Form>
+
+                {message && <Message variant = 'danger'> {message}</Message>}
+                {error && <Message variant='danger'>{error}</Message>}
+                {loading && <Loader />}
+
+                <Form onSubmit={submitHandler}>
                     <Form.Group controlId="name">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="name" placeolder="Enter Name" value ={name}></Form.Control>
+                        <Form.Control
+                            type="name"
+                            placeolder="Enter Name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        ></Form.Control>
                     </Form.Group>
-                </Form>
 
-                <Form>
                     <Form.Group controlId="email">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="text" placeolder="Enter Email" value ={email}></Form.Control>
+                        <Form.Control
+                            type="text"
+                            placeolder="Enter Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        ></Form.Control>
                     </Form.Group>
+
+                    <Form.Group controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeolder="Enter password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        ></Form.Control>
+                    </Form.Group>
+
+                    <Form.Group controlId="confirmpassword">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeolder="Enter password"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        ></Form.Control>
+                    </Form.Group>
+                    <Button type="submit" variant="primary">
+                        Update
+                    </Button>
                 </Form>
             </Col>
         </Row>
